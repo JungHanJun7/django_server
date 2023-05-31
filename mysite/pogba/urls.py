266ab5206e1f1f2e9c1b0fed7d5fc6101dfdc1b7
JungHanJun7 <jungsh4064@naver.com>
2023-05-31 23:24:31 +0900

    deploy final

--- mysite/pogba/urls.py
+++ mysite/pogba/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import postTest, testing#, alert
+from .views import postTest, testing, alert
 
 urlpatterns = [
     path("hello2/", postTest),
     path("socket/",testing),
-    #path("alert/", alert),
+    path("alert/", alert),
 ]
 
--- mysite/pogba/views.py
+++ mysite/pogba/views.py
@@ -18,83 +18,83 @@ from django.db import connection
 
 from django.db import connection
 
-# def alert(request):
-#     # 클라이언트가 GET 메소드로 요청한 경우
-#     if request.method == 'GET':
-#         context = {}
+def alert(request):
+    # 클라이언트가 GET 메소드로 요청한 경우
+    if request.method == 'GET':
+        context = {}
 
-#         # Django의 DB connection을 사용하여 cursor 객체 생성
-#         with connection.cursor() as cursor:
-#             # tcphdr 테이블과 event 테이블에서 필요한 column을 선택, cid가 같은 행을 선택
-#             cursor.execute("""
-#             SELECT t.sid, t.cid, t.tcp_sport, t.tcp_dport, t.tcp_seq, t.tcp_ack, t.tcp_flags, e.timestamp
-#             FROM tcphdr t
-#             JOIN event e ON t.cid = e.cid
-#             """)
-#             tcp_results = cursor.fetchall()
-#         # 각 행의 요소를 문자열로 변환하고, 공백으로 연결
-#         tcp_str = [' '.join(map(str, result)) for result in tcp_results]
-#          # 결과를 반전 (가장 최근의 이벤트가 위에 오도록)
-#         tcp_str.reverse()
-#         # 각 행을 개행 문자로 연결하고, 이를 컨텍스트 변수에 저장
-#         context['tcp'] = '\n'.join(tcp_str)
+        # Django의 DB connection을 사용하여 cursor 객체 생성
+        with connection.cursor() as cursor:
+            # tcphdr 테이블과 event 테이블에서 필요한 column을 선택, cid가 같은 행을 선택
+            cursor.execute("""
+            SELECT t.sid, t.cid, t.tcp_sport, t.tcp_dport, t.tcp_seq, t.tcp_ack, t.tcp_flags, e.timestamp
+            FROM tcphdr t
+            JOIN event e ON t.cid = e.cid
+            """)
+            tcp_results = cursor.fetchall()
+        # 각 행의 요소를 문자열로 변환하고, 공백으로 연결
+        tcp_str = [' '.join(map(str, result)) for result in tcp_results]
+         # 결과를 반전 (가장 최근의 이벤트가 위에 오도록)
+        tcp_str.reverse()
+        # 각 행을 개행 문자로 연결하고, 이를 컨텍스트 변수에 저장
+        context['tcp'] = '\n'.join(tcp_str)
 
-#         with connection.cursor() as cursor:
-#             cursor.execute("""
-#             SELECT t.sid, t.cid, t.udp_sport, t.udp_dport, e.timestamp
-#             FROM udphdr t
-#             JOIN event e ON t.cid = e.cid
-#             """)
-#             udp_results = cursor.fetchall()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT t.sid, t.cid, t.udp_sport, t.udp_dport, e.timestamp
+            FROM udphdr t
+            JOIN event e ON t.cid = e.cid
+            """)
+            udp_results = cursor.fetchall()
 
-#         udp_str = [' '.join(map(str, result)) for result in udp_results]
-#         udp_str.reverse()
-#         context['udp'] = '\n'.join(udp_str)
+        udp_str = [' '.join(map(str, result)) for result in udp_results]
+        udp_str.reverse()
+        context['udp'] = '\n'.join(udp_str)
 
-#         with connection.cursor() as cursor:
-#             cursor.execute("""
-#             SELECT t.sid, t.cid, t.icmp_type, e.timestamp
-#             FROM icmphdr t
-#             JOIN event e ON t.cid = e.cid
-#             """)
-#             icmp_results = cursor.fetchall()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT t.sid, t.cid, t.icmp_type, e.timestamp
+            FROM icmphdr t
+            JOIN event e ON t.cid = e.cid
+            """)
+            icmp_results = cursor.fetchall()
 
-#         icmp_str = [' '.join(map(str, result)) for result in icmp_results]
-#         icmp_str.reverse()
-#         context['icmp'] = '\n'.join(icmp_str)
+        icmp_str = [' '.join(map(str, result)) for result in icmp_results]
+        icmp_str.reverse()
+        context['icmp'] = '\n'.join(icmp_str)
 
-#         # with connection.cursor() as cursor:
-#         #     cursor.execute("""
-#         #     SELECT t.sid, t.cid, t.ip_src, t.ip_dst, ip_ttl, e.timestamp
-#         #     FROM iphdr t
-#         #     JOIN event e ON t.cid = e.cid
-#         #     """)
-#         #     ip_results = cursor.fetchall()
+        # with connection.cursor() as cursor:
+        #     cursor.execute("""
+        #     SELECT t.sid, t.cid, t.ip_src, t.ip_dst, ip_ttl, e.timestamp
+        #     FROM iphdr t
+        #     JOIN event e ON t.cid = e.cid
+        #     """)
+        #     ip_results = cursor.fetchall()
 
-#         # ip_str = [' '.join(map(str, result)) for result in ip_results]
-#         # ip_str.reverse()
-#         # context['ip'] = '\n'.join(ip_str)
-#     with connection.cursor() as cursor:
-#         cursor.execute("""
-#         SELECT t.sid, t.cid, t.ip_src, t.ip_dst, ip_ttl, e.timestamp
-#         FROM iphdr t
-#         JOIN event e ON t.cid = e.cid
-#         """)
-#         ip_results = cursor.fetchall()
+        # ip_str = [' '.join(map(str, result)) for result in ip_results]
+        # ip_str.reverse()
+        # context['ip'] = '\n'.join(ip_str)
+    with connection.cursor() as cursor:
+        cursor.execute("""
+        SELECT t.sid, t.cid, t.ip_src, t.ip_dst, ip_ttl, e.timestamp
+        FROM iphdr t
+        JOIN event e ON t.cid = e.cid
+        """)
+        ip_results = cursor.fetchall()
 
-#         ip_str = []
-#         for result in ip_results:
-#             ip_src_dec = result[2]  # Assuming ip_src is at index 2
-#             ip_dst_dec = result[3]  # Assuming ip_dst is at index 3
-#             ip_src = socket.inet_ntoa(struct.pack('!L', ip_src_dec))
-#             ip_dst = socket.inet_ntoa(struct.pack('!L', ip_dst_dec))
-#             ip_str.append(' '.join(map(str, (result[0], result[1], ip_src, ip_dst, result[4], result[5]))))
+        ip_str = []
+        for result in ip_results:
+            ip_src_dec = result[2]  # Assuming ip_src is at index 2
+            ip_dst_dec = result[3]  # Assuming ip_dst is at index 3
+            ip_src = socket.inet_ntoa(struct.pack('!L', ip_src_dec))
+            ip_dst = socket.inet_ntoa(struct.pack('!L', ip_dst_dec))
+            ip_str.append(' '.join(map(str, (result[0], result[1], ip_src, ip_dst, result[4], result[5]))))
 
-#         ip_str.reverse()
-#         context['ip'] = '\n'.join(ip_str)
+        ip_str.reverse()
+        context['ip'] = '\n'.join(ip_str)
 
 
-#         return render(request, "alert.html", context)
+        return render(request, "alert.html", context)
 
 
 
@@ -120,4 +120,6 @@ def testing(request):
         private_ip = request.POST.get('private_ip')
         initial_data = {'public_ip': public_ip, 'private_ip': private_ip}
         return render(request, "socket_test.html", initial_data)
+    else :
+        return render(request, "socket_test.html")
 
